cd63f07a4e84e6ac4c041cbdb8a8597ee592bfd8
Minuta18 <ig.samsonov10@yandex.ru>
2025-04-09 15:18:40 +0300

    webserver run

--- backend_service/app/config.py
+++ backend_service/app/config.py
@@ -7,4 +7,5 @@ class AppSettings(BaseSettings):
     used_db: DbName
     sqlite_url: typing.Optional[str]
     postgres_url: typing.Optional[str]
-    
+    app_port: int = '8000'
+    app_host: str = '0.0.0.0'
--- backend_service/main.py
+++ backend_service/main.py
@@ -1,10 +1,21 @@
 import fastapi
 import typing
+import uvicorn
+import app
 
-app = fastapi.FastAPI(__name__)
+asgi_app = fastapi.FastAPI(__name__)
 
-@app.get('/api')
+@asgi_app.get('/api')
 def index() -> typing.Any:
     return {
         'status': 'ok'
     }
+
+if __name__ == '__main__':
+    settings = app.AppSettings()
+
+    uvicorn.run(asgi_app,
+        host=settings.app_host,
+        port=settings.app_port,
+        reload=True,
+    )
